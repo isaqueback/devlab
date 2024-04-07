96d013e33db60c5a00b8a63e0d07f02f594e9ca6
Isaque Delfino <isa.quecosta00@gmail.com>
2024-04-07 20:39:06 +0100

    don't reload page when choosing same language

--- src/components/header/language-dropdown.tsx
+++ src/components/header/language-dropdown.tsx
@@ -1,6 +1,8 @@
 import Cookies from 'js-cookie'
 import { Languages } from 'lucide-react'
 
+import { useTranslationClient } from '@/hooks/use-translation/use-translation-client'
+
 import { Locale } from '../../../locales'
 import { Dropdown } from './dropdown'
 
@@ -11,6 +13,8 @@ const languages: { label: string; lang: Locale }[] = [
 ]
 
 export function LanguageDropdown() {
+  const { lang } = useTranslationClient()
+
   function handleClickLanguage(lang: Locale) {
     const expiryDate = new Date()
     expiryDate.setFullYear(expiryDate.getFullYear() + 2) // adds 2 years to the current date
@@ -26,8 +30,15 @@ export function LanguageDropdown() {
       <Dropdown.Content>
         {languages.map((language, idx) => (
           <Dropdown.ContentItem
+            className={
+              language.lang === lang
+                ? 'cursor-not-allowed bg-input hover:bg-input'
+                : ''
+            }
             key={idx}
-            onClick={() => handleClickLanguage(language.lang)}
+            onClick={() => {
+              language.lang !== lang && handleClickLanguage(language.lang)
+            }}
           >
             {language.label}
           </Dropdown.ContentItem>
