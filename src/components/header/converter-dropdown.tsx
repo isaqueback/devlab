6c9ae048e653408922b1d4d1f9f2f4e86501fd8d
Isaque Delfino <isa.quecosta00@gmail.com>
2024-04-14 15:50:25 +0100

    comment inavailable converter routes

--- src/components/header/converter-dropdown.tsx
+++ src/components/header/converter-dropdown.tsx
@@ -14,29 +14,29 @@ export function ConverterDropdown() {
 
   const colorConverters = [
     { label: t('HEX to RGB'), link: '/converter/hex-to-rgb' },
-    { label: t('RGB to HEX'), link: '/converter/rgb-to-hex' },
-    {
-      label: t('HEX with Opacity to RGBA'),
-      link: '/converter/hex-with-opacity-to-rgba',
-    },
-    {
-      label: t('RGBA to HEX with Opacity'),
-      link: '/converter/rgba-to-hex-with-opacity',
-    },
-    { label: t('HSL to HEX'), link: '/converter/hsl-to-hex' },
-    { label: t('HSL to RGB'), link: '/converter/hsl-to-rgb' },
-    { label: t('HEX to HSL'), link: '/converter/hex-to-hsl' },
-    { label: t('RGB to HSL'), link: '/converter/rgb-to-hsl' },
-    { label: t('RGBA to HSLA'), link: '/converter/rgba-to-hsla' },
-    { label: t('HSLA to RGBA'), link: '/converter/hsla-to-rgba' },
-    {
-      label: t('HEX with Opacity to HSLA'),
-      link: '/converter/hex-with-opacity-to-hsla',
-    },
-    {
-      label: t('HSLA to HEX with Opacity'),
-      link: '/converter/hsla-to-hex-with-opacity',
-    },
+    // { label: t('RGB to HEX'), link: '/converter/rgb-to-hex' },
+    // {
+    //   label: t('HEX with Opacity to RGBA'),
+    //   link: '/converter/hex-with-opacity-to-rgba',
+    // },
+    // {
+    //   label: t('RGBA to HEX with Opacity'),
+    //   link: '/converter/rgba-to-hex-with-opacity',
+    // },
+    // { label: t('HSL to HEX'), link: '/converter/hsl-to-hex' },
+    // { label: t('HSL to RGB'), link: '/converter/hsl-to-rgb' },
+    // { label: t('HEX to HSL'), link: '/converter/hex-to-hsl' },
+    // { label: t('RGB to HSL'), link: '/converter/rgb-to-hsl' },
+    // { label: t('RGBA to HSLA'), link: '/converter/rgba-to-hsla' },
+    // { label: t('HSLA to RGBA'), link: '/converter/hsla-to-rgba' },
+    // {
+    //   label: t('HEX with Opacity to HSLA'),
+    //   link: '/converter/hex-with-opacity-to-hsla',
+    // },
+    // {
+    //   label: t('HSLA to HEX with Opacity'),
+    //   link: '/converter/hsla-to-hex-with-opacity',
+    // },
   ]
 
   return (
