7d02662f7d299c56a2b7e9999c678f94f455f430
Isaque Delfino <isa.quecosta00@gmail.com>
2024-05-19 19:13:39 +0100

    add ads.txt public folder and add /converter/rgb-to-hex in sitemap.ts

--- src/app/components/google-adsense.tsx
+++ src/app/components/google-adsense.tsx
@@ -6,10 +6,13 @@ interface GoogleAdsenseProps {
 
 export function GoogleAdsense({ pId }: GoogleAdsenseProps) {
   return (
-    <Script
-      async
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
-      crossOrigin="anonymous"
-    />
+    <>
+      <meta name="google-adsense-account" content={`ca-pub-${pId}`}></meta>
+      <Script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
+        crossOrigin="anonymous"
+      />
+    </>
   )
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -12,5 +12,10 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: '2024-04-14T18:36:30+00:00',
       priority: 0.8,
     },
+    {
+      url: 'https://www.dev-lab.tech/converter/rgb-to-hex',
+      lastModified: '2024-05-19T17:55:49.330Z',
+      priority: 0.8,
+    },
   ]
 }
